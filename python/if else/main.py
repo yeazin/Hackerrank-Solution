67fc340663c9dfaa9136c2307cb74c2e38bb01f6
yeazin <farsin310yeariha9701@gmail.com>
2021-06-27 15:03:36 +0600

    ch12

--- python/if else/main.py
+++ python/if else/main.py
@@ -2,7 +2,7 @@ num = int(input())
 if num % 2 == 0 and num >= 2 and num < 5:
     print('Not Weird')
 elif num % 2 == 0 and num >= 6 and num <= 20:
-    print('Not Weird')
+    print('Weird')
 elif num % 2 == 0 and num >20:
     print ('Not Weird')
 else:
